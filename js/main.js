e2e1c3969eba3f1a4dfb309edcd7d37dd24a96dd
Jayne Clamp <jayne@greatbritaintile.com>
2025-11-16 18:11:26 -0500

    Revert to actual Jerry Joseph cover photo instead of wrong Kimberly Morgan image
    - music-cover.jpg was actually a Kimberly Morgan 2022 photo, not generic
    - Back to using the correct Jerry Joseph Flickr photo for both collection page and social sharing
    - This should show the right album cover on collections page and correct thumbnail when shared
    - May need to test if Flickr allows social media sharing of this image

--- js/main.js
+++ js/main.js
@@ -506,7 +506,7 @@ const ALBUM_DATA = {
             title: '2025-11-11 Jerry Joseph & the Jackmormons @ Nowhere Bar | Athens, GA', 
             photoCount: 11, 
             flickrUrl: 'https://www.flickr.com/photos/jayneclamp/albums/72177720330299990/',
-            coverUrl: 'https://jayne-clamp.netlify.app/images/collections/music-cover.jpg',
+            coverUrl: 'https://live.staticflickr.com/65535/54922647191_b.jpg',
             albumPage: '../music/2025-11-11-jerry-joseph-jackmormons-nowhere-bar-athens-ga.html'
         },
         { 
